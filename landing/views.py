79d0f62ea8fb3bee9baa7d53e83ddc7ddce7ffd4
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-11-03 21:14:10 -0300

    Add date and name display to home view template

--- landing/views.py
+++ landing/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
+from datetime import date
 
 
 # Create your views here.
 def home(request):
+    today = date.today()
+
     return render(
         request,
         "landing/home.html",
         {
             "title": "Home Page",
             "content": "Welcome to the Home Page!",
+            "name": "Carlos Felipe Araújo",
+            "today": today,
         },
     )
